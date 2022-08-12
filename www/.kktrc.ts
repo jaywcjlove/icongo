ef80c53a0abc55d49ff81d8d6ee5b59139452a5a
jaywcjlove <398188662@qq.com>
2022-08-12 22:24:20 +0800

    chore: split chunks code.

--- www/.kktrc.ts
+++ www/.kktrc.ts
@@ -17,6 +17,74 @@ export default (conf: WebpackConfiguration, env: 'development' | 'production', o
   );
   if (env === 'production') {
     conf.output = { ...conf.output, publicPath: './' };
+    conf.optimization = {
+      ...conf.optimization,
+      splitChunks: {
+        // maxSize: 500000,
+        cacheGroups: {
+          reactvendor: {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name: 'react-vendor',
+            chunks: 'all',
+          },
+          momentlodash: {
+            test: /[\\/]node_modules[\\/](moment-mini|lodash|d3-array|d3-geo|d3-shape|dagre)[\\/]/,
+            name: 'momentlodash-vendor',
+            chunks: 'all',
+          },
+          dagred3: {
+            test: /[\\/]node_modules[\\/](dagre-d3)[\\/]/,
+            name: 'dagre-d3-vendor',
+            chunks: 'all',
+          },
+          d3: {
+            test: /[\\/]node_modules[\\/](d3-\w+(-?\w+))[\\/]/,
+            name: 'd3-vendor',
+            chunks: 'all',
+          },
+          micromark: {
+            test: /[\\/]node_modules[\\/](micromark-\w+(-?\w+))[\\/]/,
+            name: 'micromark-vendor',
+            chunks: 'all',
+          },
+          prismjs: {
+            test: /[\\/]node_modules[\\/](refractor)[\\/]/,
+            name: 'refractor-prismjs-vendor',
+            chunks: 'all',
+          },
+          runtime: {
+            test: /[\\/]node_modules[\\/](@babel[\\/]runtime)[\\/]/,
+            name: 'babel-runtime-vendor',
+            chunks: 'all',
+          },
+          parse5: {
+            test: /[\\/]node_modules[\\/](parse5)[\\/]/,
+            name: 'parse5-vendor',
+            chunks: 'all',
+          },
+          bi: {
+            test: /[\\/](icons[\\/]bi)[\\/]/,
+            name: 'icon-bi-vendor',
+            chunks: 'all',
+          },
+          bs: {
+            test: /[\\/](icons[\\/]bs)[\\/]/,
+            name: 'icon-bs-vendor',
+            chunks: 'all',
+          },
+          di: {
+            test: /[\\/](icons[\\/]di)[\\/]/,
+            name: 'icon-di-vendor',
+            chunks: 'all',
+          },
+          sti: {
+            test: /[\\/](icons[\\/]sti)[\\/]/,
+            name: 'icon-sti-vendor',
+            chunks: 'all',
+          },
+        }
+      }
+    }
   }
   return conf;
 };
