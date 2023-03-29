b4db6a2e9a4edf6d9e030dcaf18addb50d7a78f1
jaywcjlove <398188662@qq.com>
2023-03-29 18:05:45 +0800

    fix(cb/ccp): remove attrs.

---
+++ icons/ccp/svgo.config.js
@@ -0,0 +1,25 @@
+
+/** @type {import('svgo').Config} */
+module.exports = {
+  multipass: true,
+  js2svg: {
+    indent: 2, // string with spaces or number of spaces. 4 by default
+    pretty: true, // boolean, false by default
+  },
+  plugins: [
+    'cleanupIds',
+    'removeComments',
+    'removeDimensions',
+    {
+      name: 'removeAttrs',
+      fn: (ast, params, info) => {
+        delete ast.children[0]?.attributes['id'];
+        delete ast.children[0]?.attributes['y'];
+        delete ast.children[0]?.attributes['x'];
+        delete ast.children[0]?.attributes['version'];
+        delete ast.children[0]?.attributes['style'];
+        delete ast.children[0]?.attributes['xml:space'];
+      },
+    }
+  ]
+}
